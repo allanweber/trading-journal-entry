8ea9cdb458d7cf8eefe1e4300094c6bff33a5001
Allan <a.cassianoweber@gmail.com>
2023-04-05 20:07:18 +0200

    Request scope to retrieve access token anywhere on the same thread request

--- src/main/java/com/trading/journal/entry/configuration/TokenScopeFilter.java
+++ src/main/java/com/trading/journal/entry/configuration/TokenScopeFilter.java
@@ -2,10 +2,11 @@ package com.trading.journal.entry.configuration;
 
 import com.allanweber.jwttoken.data.AccessTokenInfo;
 import com.allanweber.jwttoken.service.JwtTokenReader;
-import com.allanweber.jwttoken.service.impl.JwtResolveTokenHttpHeader;
 import com.trading.journal.entry.queries.TokenRequestScope;
 import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -18,12 +19,15 @@ import java.io.IOException;
 @Component
 public class TokenScopeFilter extends OncePerRequestFilter {
 
-    private final JwtResolveTokenHttpHeader resolveTokenHttpHeader;
     private final JwtTokenReader tokenReader;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String token = resolveTokenHttpHeader.resolve(request);
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+        String bearerToken = request.getHeader("Authorization");
+        String token = null;
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            token = bearerToken.replace("Bearer ", "");
+        }
         AccessTokenInfo accessTokenInfo = tokenReader.getAccessTokenInfo(token);
         TokenRequestScope.set(accessTokenInfo);
         filterChain.doFilter(request, response);
